b25ae85abbb2da99ccfa71dd1b4c3fb11b37ed66
USER-20170410UT\Administrator <kco1989@qq.com>
2017-05-24 10:54:18 +0800

    创建workbook和解析workbook

---
+++ src/main/java/com/kco/pattern/poi/ExcelErrorMsgBean.java
@@ -0,0 +1,41 @@
+package com.kco.pattern.poi;
+
+/**
+ * Excel解析数据信息
+ * Created by Administrator on 2017/5/24.
+ */
+public class ExcelErrorMsgBean{
+    private int row;
+    private int line;
+    private String message;
+
+    public ExcelErrorMsgBean(int row, int line, String message) {
+        this.row = row;
+        this.line = line;
+        this.message = message;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    public int getLine() {
+        return line;
+    }
+
+    public void setLine(int line) {
+        this.line = line;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
