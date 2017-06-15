06ad1753f52da307daba39e4ae8eff116ae63e23
kco1989 <kco1989@qq.com>
2017-06-15 22:11:54 +0800

    demo

---
+++ python/test/ItemTest1.py
@@ -0,0 +1,28 @@
+import scrapy
+
+class Product(scrapy.Item):
+    name = scrapy.Field()
+    price = scrapy.Field()
+    stock = scrapy.Field()
+    last_updated = scrapy.Field(serializer=str)
+
+    def test(self):
+        for i in range(10):
+            print(i)
+        for j in range(10):
+            print(j)
+
+
+if __name__ == '__main__':
+    product = Product(name="Desktop PC", price=1000)
+    print(product)
+    print(product.get('last_updated', 'no set'))
+    print('name' in product)
+    print('name1' in product)
+    print('name' in product.fields)
+    product['name'] = "Jack"
+    print(product['name'])
+    print(product.keys())
+    print(product.items())
+    for i in product.test():
+        print(i)
