dde330db98fdceaaa27778dc1bdd749520801444
lvsw <lsw@eeepay.cn>
2017-09-28 13:03:16 +0800

    angular4例子

--- staticweb/testapp/src/app/app.component.ts
+++ staticweb/testapp/src/app/app.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Hero} from "./utils/Hero";
+import {HeroService} from "./utils/HelloService"
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  ngOnInit(): void {
+    // this.getHeroes();
+    // this.getHeroesPromise();
+    this.getHerosPromiseSlowly();
+  }
+  constructor(private heroService: HeroService){}
   title = 'app';
+  heroes : Hero[];
+  hero : Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
+  selectedHero: Hero;
+  onSelect (hero: Hero):void{
+    this.selectedHero = hero;
+  }
+  getHeroes():void{
+    this.heroes = this.heroService.getHeros();
+  }
+  getHeroesPromise():void{
+    this.heroService.getHerosPromise().then(heroes => this.heroes = heroes);
+  }
+  getHerosPromiseSlowly():void{
+    this.heroService.getHerosPromiseSlowly().then(heroes => this.heroes = heroes);
+  }
 }
+
+
+
+
+// import {Component} from "@angular/core";
+// @Component({
+//   selector: 'app-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.css']
+// })
+// export class AppComponent{
+//   title = "Tour of Heroes";
+// }
--- staticweb/testapp/src/app/app.module.ts
+++ staticweb/testapp/src/app/app.module.ts
@@ -1,16 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {HeroDetailComponent} from "./detail/hero-detail.component";
+import {HeroService} from "./utils/HelloService";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
-  providers: [],
+  declarations: [
+    AppComponent,
+    HeroDetailComponent
+  ],
+  providers: [HeroService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
